d15da6d3a611524a257be120253e5e092f8ecd97
hankhank10 <m.a.hankinson@gmail.com>
2021-05-06 17:44:07 +0100

    Improve performance by not reloading logo JSON on every query

--- project/equipment_logos.py
+++ project/equipment_logos.py
@@ -10,7 +10,7 @@ def load_logos_from_json():
 
 def lookup_logo(lookup_name, look_for):
 
-    load_logos_from_json()
+    #load_logos_from_json()
     relevant_list = logo_list
 
     for logo in relevant_list:
