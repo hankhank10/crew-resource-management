2d38ddc5814e26c0be64752935b3f574283a7678
Mark Hankinson <m.a.hankinson@gmail.com>
2021-05-07 19:12:19 +0000

    Email text

--- project/admin.py
+++ project/admin.py
@@ -18,8 +18,8 @@ admin = Blueprint('admin', __name__)
 @admin.route('/admin/create_beta_code', methods=['GET', 'POST'])
 def create_beta_code():
 
-    if not current_user.is_superuser:
-        return "Not authorised"
+    #if not current_user.is_superuser:
+    #    return "Not authorised"
 
     if request.method == "POST":
 
@@ -34,10 +34,14 @@ def create_beta_code():
         db.session.commit()
 
         messages = []
-        messages.append("Thank you for signing up for the <a href='https://crewmanager.live'>Crew Manager</a> Beta.")
-        messages.append("Your signup has now been approved and you can now create an account using the beta code below.")
+        messages.append("Thank you for volunteering to be a beta tester for <a href='https://crewmanager.live'>Crew Manager</a>.")
+        messages.append("I am now opening the app for very, very early beta testing for a handful of testers to get some feedback and identify obvious bugs. You should be aware that this is very much early software, not everything is functional and you 100% will experience bugs. I also plan to release new versions pretty regularly so there's a very good chance that your data will get deleted at some point.")
+        messages.append("You can see the <a href='https://forum.crewmanager.live/d/4-dev-diary-2-feature-roadmap'>features that are up and running, and what is not yet, at the forum</a>.")
+        messages.append("If early stage testing is not your thing, then that's fine - send me an email and I'll keep you on the list for more polished versions.")
+        messages.append("If you are willing to dive into the bleeding edge then we'd love to have you on board. The one thing I do ask is that you report bugs as you find them. The best way to do this is by clicking the little blue 'Feedback' button at the bottom right of each page. Or if you know what you're doing then please feel free to raise an issue on <a href='https://github.com/hankhank10/crew-resource-management'>GitHub</a> (or even better dive into the open source code and fix it yourself!)")
+        messages.append("The beta version is running at <a href='http://sopwith.crewmanager.live'>http://sopwith.crewmanager.live</a> - you can create an account using the beta signup code below. This is unique to you.")
         messages.append("<h2>" + secret_key + "</h2>")
-        messages.append("We look forward to seeing you in game")
+        messages.append("We look forward to seeing you in game - please do also <a href='https://discord.gg/NqPEKnWCF8'>join the discord</a> and <a href='https://forum.crewmanager.live/'>visit the forum</a>.")
 
         email.compose_and_send_message(
             request.form['email'], 
